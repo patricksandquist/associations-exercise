6cbbd16c041f920b38d9736ceb585d9fc48f41f2
Patrick Sandquist <patricksandquist@gmail.com>
2015-09-09 12:06:01 -0700

    Add student course associations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,10 @@
 class Course < ActiveRecord::Base
+  has_many(
+    :enrollments,
+    class_name: "Enrollment",
+    foreign_key: :course_id,
+    primary_key: :id
+  )
+
+  has_many :enrolled_students, through: :enrollments, source: :student
 end
